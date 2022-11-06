a61b2a7224455f12cb3d5f405d2b1ec3eda8baa0
liquidz <liquidz.uo@gmail.com>
2022-11-06 16:59:40 +0900

    fix: Update ClojureMod.java to be able to specify core ns name by properties file

--- src-java/com/github/liquidz/mindustry/ClojureMod.java
+++ src-java/com/github/liquidz/mindustry/ClojureMod.java
@@ -1,28 +1,46 @@
 package com.github.liquidz.mindustry;
 
+import java.io.*;
+import java.util.Properties;
 import clojure.java.api.Clojure;
 import mindustry.mod.Mod;
 
 // https://sparkofreason.github.io/jvm-clojure-google-cloud-function/
 public class ClojureMod extends Mod {
-    public static final String core = "clj-mindustry.core";
+    public static String core = null;
+    public static final Properties properties = new Properties();
+
     static {
-        Thread.currentThread().setContextClassLoader(ClojureMod.class.getClassLoader());
-        Clojure.var("clojure.core", "require").invoke(Clojure.read(core));
+        InputStream is = ClojureMod.class.getResourceAsStream("/com/github/liquidz/mindustry/config.properties");
+        try {
+            properties.load(is);
+            core = properties.getProperty("core");
+            Thread.currentThread().setContextClassLoader(ClojureMod.class.getClassLoader());
+
+            if (core != null) {
+                Clojure.var("clojure.core", "require").invoke(Clojure.read(core));
+            }
+        } catch (IOException ex) { }
     }
 
     public ClojureMod() {
-        Clojure.var(core, "-main").invoke();
+        if (core != null) {
+            Clojure.var(core, "-main").invoke();
+        }
     }
 
     @Override
     public void init() {
-        Clojure.var(core, "-init").invoke();
+        if (core != null) {
+            Clojure.var(core, "-init").invoke();
+        }
     }
 
     @Override
     public void loadContent() {
-        Clojure.var(core, "-loadContent").invoke();
+        if (core != null) {
+            Clojure.var(core, "-loadContent").invoke();
+        }
     }
 }
 // vim:et
